63572769af5a11a626ad02e0406521102d5545c4
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-17 01:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ function driverNamesWithRevenueOver(driver, revenue) {
   })
 }
 
-function exactMatch(driver, obj) {
-  return
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let match = false;
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+    return matches;
+  });
 }
 
 function exactMatchToList(driver, obj){
