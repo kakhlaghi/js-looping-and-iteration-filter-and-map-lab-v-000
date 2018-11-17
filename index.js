7e3479fefaaade86512f797a037c745937c38add
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-17 00:26:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solution here:
 function driversWithRevenueOver(driver, revenue) {
-
+  return driver.map(function(drv){
+    return drv.revenue > revenue
+  })
 }
 
 function driverNamesWithRevenueOver(driver, revenue) {
-  
+
 }
 
 function exactMatch(driver) {
